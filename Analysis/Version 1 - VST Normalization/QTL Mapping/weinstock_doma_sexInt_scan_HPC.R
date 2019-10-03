c3c62f5a18c38040bb32d151cfc3d2f30b30dd1e
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-03 16:52:01 -0400

    Create weinstock_doma_sexInt_scan_HPC.R

---
+++ Analysis/Version 1 - VST Normalization/QTL Mapping/weinstock_doma_sexInt_scan_HPC.R
@@ -0,0 +1,22 @@
+### This shell script does sex interactive scan using the R script located at
+#      https://github.com/duytpm16/qtl2-HPC-pipeline/tree/master/R%20scripts/qtl2_scan1.R
+
+
+
+#PBS -q batch
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=24:00:00
+
+module load R/3.5.1
+
+
+viewer_data='weinstock_doma_viewer_v1.Rdata'
+dataset_expr='dataset.doma.otu|data|rz'
+num_cores='8'
+int_name='sex'
+chunk_number='NA'
+chunk_size='NA'
+
+
+
+Rscript qtl2_scan1.R viewer_data=$viewer_data dataset_expr=$dataset_expr num_cores=$num_cores int_name=$int_name chunk_number=$chunk_number chunk_size=$chunk_size
