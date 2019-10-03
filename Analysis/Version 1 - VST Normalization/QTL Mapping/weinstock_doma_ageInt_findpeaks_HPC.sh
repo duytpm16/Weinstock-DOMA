c64f7bf4246258625d856cfebdd39aa1d1090c6f
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-03 17:06:25 -0400

    Create weinstock_doma_ageInt_findpeaks_HPC.sh

---
+++ Analysis/Version 1 - VST Normalization/QTL Mapping/weinstock_doma_ageInt_findpeaks_HPC.sh
@@ -0,0 +1,23 @@
+### This script finds sex interaction peaks above 6 using R script located:
+#     https://github.com/duytpm16/qtl2-HPC-pipeline/blob/master/R%20scripts/qtl2_findpeaks.R
+
+
+#PBS -l nodes=1:ppn=8
+#PBS -q batch
+#PBS -l walltime=72:00:00
+module load R/3.5.1
+
+
+viewer_data='weinstock_doma_viewer_v1.Rdata'
+scan1_mat='doma_otu_additive_scan_v1.rds'
+dataset_expr='dataset.doma.otu|data|rz'
+thr='6'
+num_cores='8'
+type_scan='additive'
+prob='.95'
+cis_threshold='NA'
+int_mat='doma_otu_cohort.age_int_scan_v1.rds'
+
+
+
+Rscript qtl2_findpeaks.R "viewer_data=$viewer_data" "scan1_mat=$scan1_mat" "dataset_expr=$dataset_expr" "thr=$thr" "num_cores=$num_cores" "type_scan=$type_scan" "prob=$prob" "cis_threshold=$cis_threshold" "int_mat=$int_mat"
