234ef1740ba9e255059b61fd8553f38ac8160177
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-04 12:47:27 -0400

    Create weinstock_doma_estimate_heritability.R

---
+++ Analysis/Version 2 - Log Relative Abundance/QTL Mapping/weinstock_doma_estimate_heritability.R
@@ -0,0 +1,71 @@
+### Options and libraries
+options(stringsAsFactors = FALSE)
+library(qtl2)
+
+
+
+
+
+### Load viewer data and permutations
+load('weinstock_doma_viewer_v2.Rdata')
+
+
+
+
+
+### Extract data
+otu   <- dataset.doma.otu$data$norm
+covar <- dataset.doma.otu$covar.matrix
+
+
+
+
+
+
+
+### Estimate heritability per chromosome for each OTU
+chr_herit <- as.data.frame(matrix(0, nrow = 20, ncol = ncol(otu),
+                                  dimnames = list(paste0('chr',c(1:19, 'X')), colnames(otu))))
+
+for(i in colnames(chr_herit)){
+    for(j in rownames(chr_herit)){
+        chr = gsub('chr', '', j)
+        chr_herit[j, i] <- est_herit(pheno    = otu[, i, drop = FALSE], 
+                                     kinship  = K[[chr]],
+                                     addcovar = covar)[1]
+    }
+    print(i)
+}
+
+
+
+
+
+
+
+### Estimate overall heritability for each OTU
+K_overall <- calc_kinship(probs = genoprobs, type = 'overall', cores = 0)
+
+overall_herit <- as.data.frame(matrix(0, nrow = 1, ncol = ncol(otu),
+                                      dimnames = list('overall', colnames(otu))))
+
+for(i in colnames(chr_herit)){
+    overall_herit[1, i] <- est_herit(pheno = otu[, i, drop = FALSE], 
+                                     kinship  = K_overall[[chr]],
+                                     addcovar = covar)[1]
+    print(i)
+}
+
+
+
+
+
+
+
+
+### Save
+dataset.doma.otu$herit <- list(overall = overall_herit, chromsome = chr_herit)
+
+
+rm(list = ls()[!grepl('dataset[.]|genoprobs|map|markers|K', ls())])
+save.image('weinstock_doma_viewer_v2.Rdata')
