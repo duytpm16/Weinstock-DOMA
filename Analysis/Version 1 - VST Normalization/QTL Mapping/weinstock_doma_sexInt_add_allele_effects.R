94ecdf064dba75c9fb1b3660f855a20ad74d5f3d
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-04 12:24:11 -0400

    Create weinstock_doma_sexInt_add_allele_effects.R

---
+++ Analysis/Version 1 - VST Normalization/QTL Mapping/weinstock_doma_sexInt_add_allele_effects.R
@@ -0,0 +1,79 @@
+### Options and libraries
+options(stringsAsFactors = FALSE)
+library(qtl2)
+
+
+
+
+
+### Load data
+load('~/Desktop/weinstock_doma_viewer_v1.Rdata')
+ 
+
+
+
+
+
+### Extract data
+otu   <- dataset.doma.otu$data$rz
+covar <- dataset.doma.otu$covar.matrix
+peaks <- dataset.doma.otu$lod.peaks$sex_int
+samps <- dataset.doma.otu$annot.samples
+
+
+
+
+
+### Stratify by sex
+stopifnot(samps$mouse.id == rownames(otu))
+f_otu <- otu[samps$mouse.id[samps$sex == 'F'], ]
+m_otu <- otu[samps$mouse.id[samps$sex == 'M'], ]
+
+covar <- covar %>% as.data.frame() %>% select(-sex) %>% as.matrix()
+
+
+
+
+
+
+
+
+
+### Get allele effects for males and females seperately
+peaks[,paste0(LETTERS[1:8], '_female')] <- 0
+peaks[,paste0(LETTERS[1:8], '_male')] <- 0
+
+for(i in 1:nrow(peaks)){
+  
+    gp <- genoprobs[,peaks$qtl.chr[i]]
+    gp[[1]] <- gp[[1]][,,peaks$marker.id[i], drop = FALSE]
+    
+    
+    f_blup <- scan1blup(genoprobs = gp,
+                        pheno     = f_otu[,peaks$data.name[i], drop = FALSE],
+                        kinship   = K[[peaks$qtl.chr[i]]],
+                        addcovar  = covar)
+    
+    m_blup <- scan1blup(genoprobs = gp,
+                        pheno     = m_otu[,peaks$data.name[i], drop = FALSE],
+                        kinship   = K[[peaks$qtl.chr[i]]],
+                        addcovar  = covar)
+    
+    
+    peaks[i,paste0(LETTERS[1:8], '_female')] <- f_blup[1,LETTERS[1:8]]
+    peaks[i,paste0(LETTERS[1:8], '_male')]   <- m_blup[1,LETTERS[1:8]]
+    
+}
+
+
+
+
+dataset.doma.otu$lod.peaks$sex_int <- peaks
+
+
+
+
+
+### Save
+rm(list = ls()[!grepl('dataset[.]|genoprobs|map|markers|K', ls())])
+save.image('~/Desktop/weinstock_doma_viewer_v1.Rdata')
