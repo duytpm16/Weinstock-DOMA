36989a0bea6d1f5c1f720d8330595eb8176d161b
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-05 19:05:16 -0400

    Create weinstock_doma_class_format_and_normalization_v1.R

---
+++ Format and Normalization/Version 1 - VST and RankZ/weinstock_doma_class_format_and_normalization_v1.R
@@ -0,0 +1,165 @@
+### Options and libraries
+options(stringsAsFactors = FALSE)
+library(qtl2convert)
+library(tidyverse)
+library(DESeq2)
+library(qtl2)
+
+
+
+
+
+
+### Load data
+samples <- read.csv('~/Desktop/Weinstock_DOMA/Phenotypes/do_mice/DO_CrossSectional_Population.csv')
+chrY_M  <- read.csv('~/Desktop/Weinstock_DOMA/Phenotypes/do_mice/JAC_crosssectional_sex_chrM_Y_20180618.csv')
+load('~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/0.5/class_raw_count_and_taxa_0.5.Rdata')
+
+
+
+
+
+
+
+
+### Removing class with <= 5% prevalence according to Hoan
+#     class: 403 x 11
+class_counts <- class_counts[, colSums(class_counts > 0) > (nrow(class_counts) * 0.05)]
+class_taxa   <- class_taxa[,ncol(class_taxa):1]
+class_taxa   <- class_taxa %>% filter(Class %in% colnames(class_counts))
+
+
+
+
+
+
+
+
+
+### Create sample annotations
+samples <- samples %>%
+            filter(Cohort == 'Cross-Sectional') %>%
+            filter(!grepl('HDO-', Mouse.ID)) %>%
+            select(Mouse.ID, Sex, DOB, Generation, Cohort.Age, Coat.Color, Wean.Date, DOD, Age.Death, From.Cage, JCMS.Cage, Cage, Ear.Notch, Cohort) %>%
+            mutate(Mouse.ID = gsub('-', '.', Mouse.ID)) %>%
+            left_join(y = chrY_M %>% select(X, chrM, chrY) %>% dplyr::rename(Mouse.ID = X), by = 'Mouse.ID') %>%
+            filter(Mouse.ID %in% rownames(class_counts)) %>%
+            mutate(Sex = factor(Sex), Generation = factor(Generation)) %>%
+            `colnames<-`(tolower(colnames(.))) %>%
+            distinct()
+
+
+
+
+
+
+
+
+### Normalize class. 
+form <- formula(~ 1)
+dds  <- DESeqDataSetFromMatrix(countData = t(class_counts), colData  = samples, design = form) 
+vst  <- t(as.matrix(assay(varianceStabilizingTransformation(dds))))
+
+
+
+
+
+
+
+### Rank Z transformation function
+rankZ = function(x) {
+  x = rank(x, na.last = "keep", ties.method = "average") / (sum(!is.na(x)) + 1)
+  return(qnorm(x))
+} # rankZ()
+
+rz <- apply(vst,  2, rankZ)
+
+
+
+
+
+
+
+
+
+### Covariates
+covar <- model.matrix(~ sex + generation + cohort.age, data = samples)[, -1, drop = FALSE]
+covar <- covar[,c('sexM', 'cohort.age', grep('generation', colnames(covar), value = TRUE))]
+colnames(covar)[1] <- 'sex'
+rownames(covar) <- samples$mouse.id
+
+
+
+covar.info <- data.frame(sample.column = c('sex', 'cohort.age', 'generation'),
+                         covar.column  = c('sex', 'cohort.age', 'generation'),
+                         display.name  = c('Sex', 'Cohort Age', 'Generation'),
+                         interactive   = c(TRUE, TRUE, FALSE),
+                         primary       = c(TRUE, TRUE, FALSE),
+                         lod.peaks     = c('sex_int', 'age_int', NA))
+
+
+
+
+
+
+
+
+
+### Annotate columns
+annot.phenotype <- data.frame(data.name   = c(colnames(samples), class_taxa[,'Class']),
+                              short.name  = c(colnames(samples), class_taxa[,'Class']),
+                              R.name      = c(colnames(samples), class_taxa[,'Class']),
+                              description = c('Mouse identifier', 'Gender of mouse: F (female) or M (male)', 'Mouse date-of-birth', 'Generation of mouse', 'Age of mouse in months',
+                                              'Color coat of mouse', 'Date of wean', 'Date of death', 'Age at death in days', 'Cage of mouse', 'JCMS cage',
+                                              'Cage of mouse', 'Ear notch', 'JAC Cohort', 'chrM', 'chrY', apply(class_taxa, 1, paste0, collapse = ' - ')),
+                              units       = NA,
+                              category    = c(rep('Demographic', ncol(samples)), rep('Microbiome', nrow(class_taxa))),
+                              R.category  = c(rep('Demographic', ncol(samples)), rep('Microbiome', nrow(class_taxa))),
+                              is.id       = c(TRUE, rep(FALSE, ncol(samples) + nrow(class_taxa) - 1)),
+                              is.numeric  = c(FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE,
+                                              rep(TRUE, nrow(class_taxa))),
+                              is.date     = c(FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, rep(FALSE, ncol(samples) + nrow(class_taxa) - 8)),
+                              is.factor   = c(FALSE, TRUE, FALSE, TRUE, rep(FALSE, ncol(samples) + nrow(class_taxa) - 4)),
+                              factor.levels = c(NA, 'F:M', NA, '8:9:10:11:12', rep(FALSE, ncol(samples) + nrow(class_taxa) - 4)),
+                              is.covar    = c(FALSE, TRUE, FALSE, TRUE, TRUE, rep(FALSE, ncol(samples) + nrow(class_taxa) - 5)),
+                              is.pheno    = c(rep(FALSE, ncol(samples)), rep(TRUE, nrow(class_taxa))),
+                              is.derived  = FALSE,
+                              omit        = FALSE,
+                              use.covar   = c(rep(NA, ncol(samples)), rep('sex:cohort.age:generation', nrow(class_taxa))),
+                              transformation = c(rep(NA, ncol(samples)), rep('rankz', nrow(class_taxa))))
+
+
+
+
+
+
+
+
+
+### QTL viewer format
+dataset.doma.class <- list(annot.phenotype  = as_tibble(annot.phenotype),
+                           annot.samples   = as_tibble(samples),
+                           covar.matrix    = as.matrix(covar),
+                           covar.info      = as_tibble(covar.info),
+                           data            = list(norm = as.matrix(vst),
+                                                  raw  = as.matrix(class_counts),
+                                                  rz   = as.matrix(rz)),
+                           datatype        = 'phenotype',
+                           display.name    = 'DOMA Class Abundance',
+                           lod.peaks       = list(),
+                           taxa            = as_tibble(class_taxa))
+
+
+
+
+
+
+
+
+
+
+### Save
+rm(list = ls()[!grepl('dataset[.]', ls())])
+load('~/Desktop/Weinstock_DOMA/Viewer/Version 1 - VST and RankZ/weinstock_doma_viewer_v1.Rdata')
+save.image(file = '~/Desktop/Weinstock_DOMA/Viewer/Version 1 - VST and RankZ/weinstock_doma_viewer_v1.Rdata')
+
