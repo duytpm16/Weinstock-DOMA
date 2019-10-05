5de51b6e8b1c19b45d6297c890d8ecbddd4041e4
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-05 18:32:32 -0400

    Update weinstock_doma_otu_clean_and_filter.R

--- Format and Normalization/Raw OTU Counts/weinstock_doma_otu_clean_and_filter.R
+++ Format and Normalization/Raw OTU Counts/weinstock_doma_otu_clean_and_filter.R
@@ -10,7 +10,8 @@ library(qtl2)
 ### Read in OTU data
 taxa_0.8 <- read.delim(file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/otu_taxa_rdpc0.8.tsv')
 taxa_0.5 <- read.delim(file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/otu_taxa_rdpc0.5.tsv')
-otu  <- read.delim(file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/otu_table_by_otutab.tsv')
+otu_0.8  <- read.delim(file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/otu_table_by_otutab_0.8.tsv')
+otu_0.5  <- read.delim(file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/DOMA_otu_403_0.5.txt')
 
 
 
@@ -44,37 +45,32 @@ taxa_0.5 <- as.data.frame(taxa_0.5)
 
 
 
-### Editing OTU table
-otu <- otu %>%
-          mutate(X.OTU.ID = gsub('_', '-', X.OTU.ID)) %>%
-          column_to_rownames('X.OTU.ID')
+### Editing OTU 0.8 table
+otu_0.8 <- otu_0.8 %>%
+              mutate(X.OTU.ID = gsub('_', '-', X.OTU.ID)) %>%
+              column_to_rownames('X.OTU.ID')
 
 
-orig.id <- colnames(otu)
-colnames(otu) <- gsub('DOMA_J00[0-9][A-Z][A-Z]_1_ST_T0_B0_0000_|DOMA_J00[0-9][A-Z][0-9]_1_ST_T0_B0_0000_|DOMA_J00[0-9][0-9][A-Z]_1_ST_T0_B0_0000_','',colnames(otu))
-colnames(otu) <- gsub('(DO_[0-9][0-9][0-9][0-9]).*', '\\1', colnames(otu))
-colnames(otu) <- gsub('(D0_[0-9][0-9][0-9][0-9]).*', '\\1', colnames(otu))
-colnames(otu) <- gsub('D0', 'DO', colnames(otu))
-
-
-
-
-
+orig.id <- colnames(otu_0.8)
+colnames(otu_0.8) <- gsub('DOMA_J00[0-9][A-Z][A-Z]_1_ST_T0_B0_0000_|DOMA_J00[0-9][A-Z][0-9]_1_ST_T0_B0_0000_|DOMA_J00[0-9][0-9][A-Z]_1_ST_T0_B0_0000_','',colnames(otu_0.8))
+colnames(otu_0.8) <- gsub('(DO_[0-9][0-9][0-9][0-9]).*', '\\1', colnames(otu_0.8))
+colnames(otu_0.8) <- gsub('(D0_[0-9][0-9][0-9][0-9]).*', '\\1', colnames(otu_0.8))
+colnames(otu_0.8) <- gsub('D0', 'DO', colnames(otu_0.8))
 
 
 
 ### Removing duplicates. Keep the one with with highest abundance according to Dong-Binh
-dups <- unique(colnames(otu)[duplicated(colnames(otu))])
-keep <- rep(TRUE, ncol(otu))
+dups <- unique(colnames(otu_0.8)[duplicated(colnames(otu_0.8))])
+keep <- rep(TRUE, ncol(otu_0.8))
 for(i in dups){
   
-    wh <- which(colnames(otu) == i)
-    lessZeros <- which.min(colSums(otu[,wh] == 0))
-    totalSum  <- which.max(colSums(otu[,wh]))
+    wh <- which(colnames(otu_0.8) == i)
+    lessZeros <- which.min(colSums(otu_0.8[,wh] == 0))
+    totalSum  <- which.max(colSums(otu_0.8[,wh]))
   
     keep[wh[-totalSum]] <- FALSE
 }
-otu <- otu[,keep]
+otu_0.8 <- otu_0.8[,keep]
 orig.id <- orig.id[keep]
 
 
@@ -86,13 +82,30 @@ orig.id <- orig.id[keep]
 
 
 
+
+### Editing OTU 0.5 table
+otu_0.5 <- otu_0.5 %>% 
+             dplyr::mutate(SampleID = gsub('_', '-', SampleID)) %>%  
+             arrange(SampleID) %>%
+             column_to_rownames('SampleID')
+colnames(otu_0.5) <- gsub('(DO_[0-9][0-9][0-9][0-9]).*', "\\1", colnames(otu_0.5))
+colnames(otu_0.5) <- gsub('_', ".", colnames(otu_0.5))
+
+
+
+
+
+
+
+
 ### Reorder
-otu <- otu[taxa_0.8$OTU, order(colnames(otu))]
-colnames(otu) <- gsub('_', '.', colnames(otu))
-otu <- t(otu)
+otu_0.8 <- otu_0.8[taxa_0.8$OTU, order(colnames(otu_0.8))]
+colnames(otu_0.8) <- gsub('_', '.', colnames(otu_0.8))
+otu_0.8 <- t(otu_0.8)
 
 
-
+otu_0.5 <- otu_0.5[taxa_0.5$OTU, order(colnames(otu_0.5))]
+otu_0.5 <- t(otu_0.5)
 
 
 
@@ -104,4 +117,5 @@ otu <- t(otu)
 ### Save
 saveRDS(taxa_0.8, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.8_cleaned.rds')
 saveRDS(taxa_0.5, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.5_cleaned.rds')
-saveRDS(otu, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_raw_count_cleaned.rds')
+saveRDS(otu_0.5, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_raw_count_0.5_cleaned.rds')
+saveRDS(otu_0.8, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_raw_count_0.8_cleaned.rds')
