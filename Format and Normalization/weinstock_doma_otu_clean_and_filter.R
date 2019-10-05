adc885b1201f6b51208304c4162db14a1f57d8cd
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-05 14:17:32 -0400

    Update weinstock_doma_otu_clean_and_filter.R

--- Format and Normalization/weinstock_doma_otu_clean_and_filter.R
+++ Format and Normalization/weinstock_doma_otu_clean_and_filter.R
@@ -20,7 +20,7 @@ otu  <- read.delim(file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data
 
 ### Editing taxa table
 taxa_0.8 <- taxa_0.8 %>% 
-              rename(OTU = X) %>%
+              dplyr::rename(OTU = X) %>%
               mutate(OTU = gsub('_', '-', OTU)) %>%
               select(OTU, genus, family, order, class, phylum, domain)
 taxa_0.8 <- apply(taxa_0.8, 2, function(x) gsub('_', ' ', x))
@@ -29,9 +29,9 @@ taxa_0.8 <- as.data.frame(taxa_0.8)
 
 
 taxa_0.5 <- taxa_0.5 %>% 
-                rename(OTU = X) %>%
-                mutate(OTU = gsub('_', '-', OTU)) %>%
-                select(OTU, genus, family, order, class, phylum, domain)
+              dplyr::rename(OTU = X) %>%
+              mutate(OTU = gsub('_', '-', OTU)) %>%
+              select(OTU, genus, family, order, class, phylum, domain)
 taxa_0.5 <- apply(taxa_0.5, 2, function(x) gsub('_', ' ', x))
 taxa_0.5 <- as.data.frame(taxa_0.5)
 
@@ -46,8 +46,8 @@ taxa_0.5 <- as.data.frame(taxa_0.5)
 
 ### Editing OTU table
 otu <- otu %>%
-        mutate(X.OTU.ID = gsub('_', '-', X.OTU.ID)) %>%
-        column_to_rownames('X.OTU.ID')
+          mutate(X.OTU.ID = gsub('_', '-', X.OTU.ID)) %>%
+          column_to_rownames('X.OTU.ID')
 
 
 orig.id <- colnames(otu)
@@ -69,7 +69,6 @@ keep <- rep(TRUE, ncol(otu))
 for(i in dups){
   
     wh <- which(colnames(otu) == i)
-    print(wh)
     lessZeros <- which.min(colSums(otu[,wh] == 0))
     totalSum  <- which.max(colSums(otu[,wh]))
   
@@ -81,62 +80,28 @@ orig.id <- orig.id[keep]
 
 
 
-                  
-                  
-                  
-                  
-                  
-### Keep raw
-raw <- otu
-raw_taxa_0.5 <- taxa_0.5
-raw_taxa_0.8 <- taxa_0.8
-
-
-
-
-                  
-                  
-                  
-                  
-                  
-### Remove OTUs with prevalence > 5%
-otu  <- otu[which(rowSums(otu > 0) > (ncol(otu) * 0.05)),]
-taxa_0.8 <- taxa_0.8 %>% filter(OTU %in% rownames(otu))
-taxa_0.5 <- taxa_0.5 %>% filter(OTU %in% rownames(otu))
 
 
 
 
 
-                 
-                  
-                  
-                  
-                  
+
 ### Reorder
 otu <- otu[taxa_0.8$OTU, order(colnames(otu))]
 colnames(otu) <- gsub('_', '.', colnames(otu))
 otu <- t(otu)
 
 
-raw <- raw[raw_taxa_0.8$OTU, order(colnames(raw))]
-colnames(raw) <- gsub('_', '.', colnames(raw))
-raw <- t(raw)
 
 
 
 
 
-                  
-                  
-                  
 
 
-                  
+
+
 ### Save
-saveRDS(raw_taxa_0.8, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.8_cleaned.rds')
-saveRDS(raw_taxa_0.5, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.5_cleaned.rds')
-saveRDS(taxa_0.8, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.8_cleaned_filtered.rds')
-saveRDS(taxa_0.5, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.5_cleaned_filtered.rds')
-saveRDS(otu, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_raw_count_cleaned_filtered_0.05.rds')
-saveRDS(raw, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_raw_count_cleaned.rds')
+saveRDS(taxa_0.8, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.8_cleaned.rds')
+saveRDS(taxa_0.5, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_taxa_table_0.5_cleaned.rds')
+saveRDS(otu, file = '~/Desktop/Weinstock_DOMA/Phenotypes/doma_otu_16s_data/Modified/otu_raw_count_cleaned')
