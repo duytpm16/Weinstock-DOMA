700e125a3bcd9c1e56e18c5aa0b2e86f4bcfed79
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-05 19:48:43 -0400

    Create doma_order_estimate_heritability.R

---
+++ Analysis/Version 1 - VST Normalization/QTL Analysis/Order/doma_order_estimate_heritability.R
@@ -0,0 +1,89 @@
+### Options and libraries
+options(stringsAsFactors = FALSE)
+library(qtl2)
+
+
+
+
+
+### Load viewer data and permutations
+load('~/Desktop/Weinstock_DOMA/Viewer/Version 1 - VST and RankZ/weinstock_doma_viewer_v1.Rdata')
+
+
+
+
+
+
+
+
+
+### Extract data
+order <- dataset.doma.order$data$rz
+covar <- dataset.doma.order$covar.matrix
+
+
+
+
+
+
+
+
+
+### Estimate heritability per chromosome for each order
+chr_herit <- as.data.frame(matrix(0, nrow = 20, ncol = ncol(order),
+                                  dimnames = list(paste0('chr',c(1:19, 'X')), colnames(order))))
+
+for(i in colnames(chr_herit)){
+    for(j in rownames(chr_herit)){
+        chr = gsub('chr', '', j)
+        chr_herit[j, i] <- est_herit(pheno    = order[, i, drop = FALSE], 
+                                     kinship  = K[[chr]],
+                                     addcovar = covar,
+                                     cores    = 0)[1]
+    }
+    print(i)
+}
+
+
+
+
+
+
+
+
+
+### Estimate overall heritability for each order
+K_overall <- calc_kinship(probs = genoprobs, type = 'overall', cores = 0)
+
+overall_herit <- as.data.frame(matrix(0, nrow = 1, ncol = ncol(order),
+                                      dimnames = list('overall', colnames(order))))
+
+for(i in colnames(overall_herit)){
+    overall_herit[1, i] <- est_herit(pheno    = order[, i, drop = FALSE], 
+                                     kinship  = K_overall,
+                                     addcovar = covar,
+                                     cores    = 0)[1]
+    print(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Save
+dataset.doma.order$herit <- list(overall = overall_herit, 
+                                 chromosome = chr_herit)
+
+
+rm(list = ls()[!grepl('dataset[.]|genoprobs|map|markers|K', ls())])
+rm(K_overall)
+save.image('~/Desktop/Weinstock_DOMA/Viewer/Version 1 - VST and RankZ/weinstock_doma_viewer_v1.Rdata')
