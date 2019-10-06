05210561858bfd083ae438964a2af7f48df07e1a
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-05 21:38:52 -0400

    Create doma_genus_ageInt_scan1_HPC_v1.sh

---
+++ Analysis/Version 1 - VST Normalization/QTL Analysis/Genus/doma_genus_ageInt_scan1_HPC_v1.sh
@@ -0,0 +1,21 @@
+### This shell script runs age interaction scan using R script from:
+#      https://github.com/duytpm16/qtl2-HPC-pipeline/blob/master/R%20scripts/qtl2_scan1.R
+
+
+#PBS -q batch
+#PBS -l nodes=1:ppn=2
+#PBS -l walltime=24:00:00
+
+
+module load R/3.5.1
+
+
+viewer_data='weinstock_doma_viewer_v1.Rdata'
+dataset_expr='dataset.doma.genus|data|rz'
+num_cores='2'
+int_name='cohort.age'
+chunk_number='NA'
+chunk_size='NA'
+
+
+Rscript qtl2_scan1.R viewer_data=$viewer_data dataset_expr=$dataset_expr num_cores=$num_cores int_name=$int_name chunk_number=$chunk_number chunk_size=$chunk_size
