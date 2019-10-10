e43c95979320465f9af5034be9fa5ef57d5a5f70
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-10 11:09:15 -0400

    Create weinstock_doma_additive_1000_perm_summary.R

---
+++ Analysis/Version 2 - Log Relative Abundance/QTL Mapping/weinstock_doma_additive_1000_perm_summary.R
@@ -0,0 +1,75 @@
+### Options and libraries
+options(stringsAsFactors = FALSE)
+library(qtl2)
+
+
+
+
+
+
+
+### Load viewer
+#load('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/weinstock_doma_viewer_v2.Rdata')
+
+
+
+
+
+
+
+
+### Load in additive permutations
+otu_perm    <- readRDS('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/Permutation Matrix/doma_otu_additive_perm_1000_v2.rds')
+genus_perm  <- readRDS('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/Permutation Matrix/doma_genus_additive_perm_1000_v2.rds')
+family_perm <- readRDS('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/Permutation Matrix/doma_family_additive_perm_1000_v2.rds')
+order_perm  <- readRDS('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/Permutation Matrix/doma_order_additive_perm_1000_v2.rds')
+class_perm  <- readRDS('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/Permutation Matrix/doma_class_additive_perm_1000_v2.rds')
+phylum_perm <- readRDS('~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/Permutation Matrix/doma_phylum_additive_perm_1000_v2.rds')
+
+
+
+
+
+
+
+
+
+### Permutation summaries
+otu_summary    <- summary_scan1perm(object = otu_perm,    alpha = c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.67))
+genus_summary  <- summary_scan1perm(object = genus_perm,  alpha = c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.67))
+family_summary <- summary_scan1perm(object = family_perm, alpha = c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.67))
+order_summary  <- summary_scan1perm(object = order_perm,  alpha = c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.67))
+class_summary  <- summary_scan1perm(object = class_perm,  alpha = c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.67))
+phylum_summary <- summary_scan1perm(object = phylum_perm, alpha = c(0.001, 0.01, 0.05, 0.1, 0.15, 0.2, 0.67))
+
+
+
+
+
+
+
+
+
+### Save additive permutations
+dataset.doma.otu$perms    <- list(additive = otu_summary)
+dataset.doma.genus$perms  <- list(additive = genus_summary)
+dataset.doma.family$perms <- list(additive = family_summary)
+dataset.doma.order$perms  <- list(additive = order_summary)
+dataset.doma.class$perms  <- list(additive = class_summary)
+dataset.doma.phylum$perms <- list(additive = phylum_summary)
+
+
+
+
+
+
+
+
+
+### Save
+rm(list = ls()[!grepl('dataset[.]|genoprobs|K|map|markers', ls())])
+save.image(file = '~/Desktop/Weinstock_DOMA/Viewer/Version 2 - Centered Log Ratio/weinstock_doma_viewer_v2.Rdata')
+
+
+
+
